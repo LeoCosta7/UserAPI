baf0808bb264de4f50a3496bb38df17280cd7f98
Leonardo <lcampelo17@gmail.com>
2023-06-28 20:20:20 -0300

    Criação de controller; Ajuste no DbContext

---
+++ UserAPI/Controllers/UserManagerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAPI.Models;
+
+namespace UserAPI.Controllers
+{
+    [ApiController]
+    [Route("Api/controller")]
+    public class UserManagerController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            return null;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser()
+        {
+            return null;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser()
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser()
+        {
+            return null;
+        }
+    }
+}
