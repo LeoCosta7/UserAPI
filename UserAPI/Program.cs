cc6b4266a6f5a1f7f6957921d1d0079ca4a5e5b1
Leonardo <lcampelo17@gmail.com>
2023-06-28 20:23:07 -0300

    Ajustes

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Add services to the container.
 
