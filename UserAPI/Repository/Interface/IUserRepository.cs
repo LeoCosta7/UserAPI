9baf81e5b89942952ed680779a4a268abb7cf6ff
Leonardo <lcampelo17@gmail.com>
2023-07-05 17:25:06 -0300

    Criação do Repository

---
+++ UserAPI/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAPI.Models;
+
+namespace UserAPI.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+
+        Task<User> GetById(int id);
+
+        void AddUser(User user);
+
+        void UpdateUser(User user);
+
+        void DeleteUser(User user);
+    }
+}
---
+++ UserAPI/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using UserAPI.Models;
+
+namespace UserAPI.Repository
+{
+    public class UserRepository
+    {
+        public Task<IEnumerable<User>> GetAllUsers()
+        {
+            return null;
+        }
+
+        public Task<User> GetById(int id)
+        {
+            return null;
+        }
+
+        public void AddUser(User user)
+        {
+
+        }
+
+        public void UpdateUser(User user)
+        {
+
+        }
+
+        public void DeleteUser(User user)
+        {
+
+        }
+    }
+}
